871278fb732ab2ba3212c6a631e3db48089e7f2c
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-02-03 13:50:56 +0530

    Create calcy.groovy

---
+++ vars/calcy.groovy
@@ -0,0 +1,13 @@
+def add(x,y,z){
+  print "Sum of ${x} , ${y} & $ {z} is ${x+y+z}"
+}
+def sub(a,b){
+  print "substraction of ${a} and ${b} is ${a-b}"
+}
+def mul(x,y,z){
+  print "Multiplication of ${x} , ${y} & ${z} is ${x*y*z}"
+}
+
+def div(a,b){
+  print "Division of ${a} & ${b} is ${a/b}
+}
