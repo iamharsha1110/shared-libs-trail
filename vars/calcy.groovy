21049ca09374e81466ad487eac5a0c3d3e7ce59a
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-02-03 13:53:11 +0530

    Update calcy.groovy

--- vars/calcy.groovy
+++ vars/calcy.groovy
@@ -1,5 +1,5 @@
 def add(x,y,z){
-  print "Sum of ${x} , ${y} & $ {z} is ${x+y+z}"
+  print "Sum of ${x} , ${y} & ${z} is ${x+y+z}"
 }
 def sub(a,b){
   print "substraction of ${a} and ${b} is ${a-b}"
