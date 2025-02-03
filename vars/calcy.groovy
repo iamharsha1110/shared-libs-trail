89e80e608f2f675d1ff61183c9281f3be5dc9fe2
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-02-03 13:55:49 +0530

    Update calcy.groovy

--- vars/calcy.groovy
+++ vars/calcy.groovy
@@ -1,13 +1,19 @@
-def add(x,y,z){
-  print "Sum of ${x} , ${y} & ${z} is ${x+y+z}"
-}
-def sub(a,b){
-  print "substraction of ${a} and ${b} is ${a-b}"
-}
-def mul(x,y,z){
-  print "Multiplication of ${x} , ${y} & ${z} is ${x*y*z}"
+def add(x, y, z) {
+    print "Sum of ${x}, ${y} & ${z} is ${x + y + z}"
 }
 
-def div(a,b){
-  print "Division of ${a} & ${b} is ${a/b}
+def sub(a, b) {
+    print "Subtraction of ${a} and ${b} is ${a - b}"
+}
+
+def mul(x, y, z) {
+    print "Multiplication of ${x}, ${y} & ${z} is ${x * y * z}"
+}
+
+def div(a, b) {
+    if (b == 0) {
+        print "Division by zero is undefined"
+    } else {
+        print "Division of ${a} & ${b} is ${a / b}"
+    }
 }
