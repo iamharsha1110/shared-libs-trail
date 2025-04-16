eca23b7b70d3274f6ae26bde904a3a40307be563
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-16 11:55:47 +0530

    Create custom.groovy

---
+++ vars/custom.groovy
@@ -0,0 +1,8 @@
+def call(String repoName) {
+    def gitUrl = "https://github.com/iamharsha1110/${repoName}.git"
+    sh """
+        git clone ${gitUrl}
+        cd ${repoName}
+        mvn clean package
+    """
+}
