76fbf3667b0ad262907a7e9f2ebbd1c825cfd4d1
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-17 10:56:46 +0530

    Update custom.groovy

--- vars/custom.groovy
+++ vars/custom.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     def gitUrl = "https://github.com/iamharsha1110/${config.repoName}.git"
     sh """
         git clone -b ${config.branch} ${gitUrl}
-        cd ${repoName}
+        cd ${config.repoName}
         mvn clean compile
         echo "Cloning done - ${config.repoName} from ${gitUrl} on branch '${config.branch}'"
     """
