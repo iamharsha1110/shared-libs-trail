c12d137e392b71b742d58d0313364ed59dc929d9
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-20 16:17:37 +0530

    Delete vars/git-checkout.groovy

--- vars/git-checkout.groovy
+++
@@ -1,8 +0,0 @@
-def call(String repoName, String branch) {
-    def gitUrl = "https://github.com/iamharsha1110/${repoName}.git"
-    sh """
-        git clone -b ${branch} ${gitUrl}
-        cd ${repoName}
-        mvn clean package
-    """
-}
