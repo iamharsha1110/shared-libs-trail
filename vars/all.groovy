1da362adcac7da1dee0f16b75c7793c6b3693f91
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-21 16:09:00 +0530

    Create all.groovy

---
+++ vars/all.groovy
@@ -0,0 +1,48 @@
+def gc(Map config = [:]) {
+    def gitUrl = "https://github.com/iamharsha1110/${config.repoName}.git"
+    sh """
+        git clone -b ${config.branch} ${gitUrl}
+        echo "Cloning done - ${config.repoName} from ${gitUrl} on branch '${config.branch}'"
+        pwd
+        cd ${config.repoName}
+        pwd
+    """
+}
+
+def test(Map config = [:]) {
+    sh """
+        cd ${config.repoName}
+        mvn test
+    """
+}
+
+def sonar(Map config = [:]) {
+    withSonarQubeEnv(config.sonarserver) {
+        withCredentials([string(credentialsId: config.sonartoken, variable: 'SONAR_TOKEN')]) {
+            sh """
+               cd ..
+               cd ${config.pipelinename}
+               cd ${config.repoName}
+               pwd
+                mvn clean verify -DskipTests=true sonar:sonar \
+                    -Dsonar.projectKey=${config.projectKey} \
+                    -Dsonar.projectName='${config.projectName}' \
+                    -Dsonar.token=$SONAR_TOKEN
+            """
+        }
+    }
+}
+
+def qg(Map config = [:]) {
+    timeout(time: ${config.timeout}, unit: 'MINUTES') {
+        waitForQualityGate abortPipeline: true
+    }
+}
+
+def build(Map config = [:]) {
+    sh """
+        pwd
+        cd ${config.repoName}
+        mvn clean package
+        """
+}
