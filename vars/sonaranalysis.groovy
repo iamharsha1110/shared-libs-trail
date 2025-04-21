0bf28cb1e344cd7312e348f2a171c41048492948
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-21 14:13:40 +0530

    Update sonaranalysis.groovy

--- vars/sonaranalysis.groovy
+++ vars/sonaranalysis.groovy
@@ -2,9 +2,8 @@ def call(Map config = [:]) {
     withSonarQubeEnv(config.sonarserver) {
         withCredentials([string(credentialsId: config.sonartoken, variable: 'SONAR_TOKEN')]) {
             sh """
-               cd ..
-               cd ${config.pipelinename}
-               cd ${config.repoName}
+               cd
+               cd "var/lib/jenkins/workspace/${config.foldername}/${config.pipelinename}/${config.repoName}
                pwd
                 mvn clean verify -DskipTests=true sonar:sonar \
                     -Dsonar.projectKey=${config.projectKey} \
