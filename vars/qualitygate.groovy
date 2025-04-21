dc65d6f5cc07e904d9d72261e9276ea07710ab1e
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-21 15:40:04 +0530

    Update qualitygate.groovy

--- vars/qualitygate.groovy
+++ vars/qualitygate.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    timeout(time: ${config.timeout}, unit: 'MINUTES') {
+    timeout(time: config.timeout ?: 2, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: true
     }
 }
