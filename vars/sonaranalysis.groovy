1aa2e0f050105a64f1a90b0681ebb814da4e2b36
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-22 14:04:57 +0530

    Update sonaranalysis.groovy

--- vars/sonaranalysis.groovy
+++ vars/sonaranalysis.groovy
@@ -1,12 +1,9 @@
-def call(Map config = [:]) {
-    def path = "../${config.pipelinename}/${config.repoName}"
-    
+def sonar(Map config = [:]) {
     withSonarQubeEnv(config.sonarserver) {
         withCredentials([string(credentialsId: config.sonartoken, variable: 'SONAR_TOKEN')]) {
-            dir(path) {
+            dir("${config.repoName}") {
                 sh """
                     pwd
-                    echo "\${SONAR_TOKEN}"
                     mvn clean verify -DskipTests=true sonar:sonar \\
                         -Dsonar.projectKey=${config.projectKey} \\
                         -Dsonar.projectName='${config.projectName}' \\
