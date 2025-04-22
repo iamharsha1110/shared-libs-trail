e374a38b5107161171a55b6da2eb61efdc42de6e
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-22 12:44:30 +0530

    Update all.groovy

--- vars/all.groovy
+++ vars/all.groovy
@@ -20,6 +20,7 @@ def sonar(Map config = [:]) {
         withCredentials([string(credentialsId: config.sonartoken, variable: 'SONAR_TOKEN')]) {
             dir("${config.repoName}") {
                 sh """
+                    pwd
                     mvn clean verify -DskipTests=true sonar:sonar \\
                         -Dsonar.projectKey=${config.projectKey} \\
                         -Dsonar.projectName='${config.projectName}' \\
