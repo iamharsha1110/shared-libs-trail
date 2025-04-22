55534c28e7eb1fdadfbd0f33b77592f30e7c7d7b
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-04-22 11:28:12 +0530

    Update all.groovy

--- vars/all.groovy
+++ vars/all.groovy
@@ -34,8 +34,8 @@ def sonar(Map config = [:]) {
     }
 }
 
-def qg(Map config = [:]) {
-    timeout(time: ${config.timeout}, unit: 'MINUTES') {
+def call(Map config = [:]) {
+    timeout(time: config.timeout ?: 2, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: true
     }
 }
